825e64eee28008b6790321f5d63c0b3889c372b5
Vivek Shah <vivekshahcodes@gmail.com>
2023-06-20 20:17:40 +0530

    Initial commit

---
+++ backend/src/test/java/com/vivek/fitnesstracker/FitnessTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.fitnesstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
